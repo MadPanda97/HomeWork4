b3e2d1852cc9c1a7af07b2768479e456341fa3c7
MadPanda97 <19zainollinov97@gmail.com>
2025-03-20 03:48:22 +0800

    3V

--- main.go
+++ main.go
@@ -1,70 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func longestCommonPrefix(strs []string) []rune {
+	if len(strs) == 0 {
+		return []rune{}
+	}
+
+	prefix := []rune{}
+
+search:
+	for i := 0; i < len(strs[0]); i++ {
+		V := strs[0][i]
+		for j := 1; j < len(strs); j++ {
+			if i >= len(strs[j]) || strs[j][i] != V {
+				break search
+			}
+		}
+		prefix = append(prefix, rune(V))
+	}
+	return prefix
+}
 
 func main() {
-	var (
-		string1 = "hi, how are u?"
-		string2 = "hi, Arni"
-	)
-	var LongestCommonPrefix []rune
-
-	MinLength := len(string1)
-	if len(string2) < MinLength {
-		MinLength = len(string2)
-	}
-
-	for i := 0; i < MinLength; i++ {
-		if string1[i] != string2[i] {
-			break
-
-		}
-		LongestCommonPrefix = append(LongestCommonPrefix, rune(string1[i]))
-	}
-	fmt.Println(string(LongestCommonPrefix))
-	matrix := [3][4]int{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}}
-
-	var zeroRows, zeroCols []int
-
-	for i, row := range matrix {
-		for j, v := range row {
-			if v == 0 {
-				zeroRows = append(zeroRows, i)
-				zeroCols = append(zeroCols, j)
-			}
-		}
-	}
-	for _, row := range zeroRows {
-		for j := 0; j < len(matrix[row]); j++ {
-			matrix[row][j] = 0
-		}
-	}
-
-	for _, col := range zeroCols {
-		for i := 0; i < len(matrix); i++ {
-			matrix[i][col] = 0
-		}
-	}
-	for _, row := range matrix {
-		fmt.Println(row)
-	}
-
-	var arrnums = [9]int{1, 5, 4, 4, 1, 8, 8, 7, 5}
-	n := len(arrnums)
-	for i := 0; i < n; i++ {
-		unique := true
-
-		for j := 0; j < n; j++ {
-			if i != j && arrnums[i] == arrnums[j] {
-				unique = false
-				break
-			}
-		}
-		if unique {
-			println(arrnums[i])
-
-		}
-
-	}
-
+	A := []string{"butterfly", "butter", "buttress"}
+	fmt.Println(string(longestCommonPrefix(A)))
 }
